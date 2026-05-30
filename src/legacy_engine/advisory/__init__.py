91d85333ca29e3ca26d3c0c7b0b43918ea03ce73
andromedus1 <andr0m3dus@gmail.com>
2026-05-30 16:01:35 -0600

    implement: epic-advisory-positioning

--- src/legacy_engine/advisory/__init__.py
+++ src/legacy_engine/advisory/__init__.py
@@ -6,10 +6,22 @@ from legacy_engine.advisory.field import (
     build_custom_field,
     build_global_field,
 )
+from legacy_engine.advisory.positioning import (
+    DeckRanking,
+    PositioningResult,
+    delta_var_S,
+    positioning_score,
+    rank_decks,
+)
 
 __all__ = [
     "FieldDistribution",
     "FieldSource",
     "build_custom_field",
     "build_global_field",
+    "DeckRanking",
+    "PositioningResult",
+    "delta_var_S",
+    "positioning_score",
+    "rank_decks",
 ]
